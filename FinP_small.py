2fb48552d930cb8653a5761dba1d35fa82e87b9f
bjtbgbg <143383219+bjtbgbg@users.noreply.github.com>
2026-05-20 01:57:26 +0800

    Update FinP_small.py

--- FinP_small.py
+++ FinP_small.py
@@ -11,6 +11,21 @@ client = OpenAI(
     base_url=base_url,
 )
 
-completion = client.chat.completions.create(model=path, messages=[{'role': 'user', 'content': question}])
+completion = client.chat.completions.create(
+    model=path,
+    messages=[
+        {
+            "role": "user",
+            "content": question
+        }
+    ],
+    temperature=0.8,
+    top_p=0.95,
+    extra_body={
+        "top_k": 50,
+        "min_p": 0.0,
+    },
+)
+
 response = json.loads(completion.model_dump_json())
 print(response['choices'][0]['message']['content'])
