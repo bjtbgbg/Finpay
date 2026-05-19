669e3da9d4d864750613b5b452ae456d2c58c4c6
bjtbgbg <143383219+bjtbgbg@users.noreply.github.com>
2026-05-20 02:31:52 +0800

    Update FinP_large.py

--- FinP_large.py
+++ FinP_large.py
@@ -1,9 +1,9 @@
 import json
 from openai import OpenAI
 
-api_key = "xxxxxxxx"
+api_key = "xxxxxxxxxxxxx"  
 base_url = "https://chat.intern-ai.org.cn/api/v1/"
-path = "intern-s1"
+path = "intern-s1"  
 question = "你好"
 
 client = OpenAI(
@@ -11,6 +11,24 @@ client = OpenAI(
     base_url=base_url,
 )
 
-completion = client.chat.completions.create(model=path, messages=[{'role': 'user', 'content': question}])
+completion = client.chat.completions.create(
+    model=path,
+    messages=[
+        {
+            "role": "user",
+            "content": question
+        }
+    ],
+    temperature=0.7,
+    top_p=0.95,
+    extra_body={
+        "top_k": 50,
+        "repetition_penalty": 1.0,
+        "enable_thinking": True,
+    },
+)
+
 response = json.loads(completion.model_dump_json())
-print(response['choices'][0]['message']['content'])
+content = response["choices"][0]["message"]["content"]
+
+print(content)
